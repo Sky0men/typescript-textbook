a37858c62c7b5fb77814e0fac2aa5e2bd20e8cbd
Ivan Zelentsov <vanya.zelenczov@mail.ru>
2024-02-29 11:33:48 +0300

    методы

--- app.ts
+++ app.ts
@@ -1,18 +1,35 @@
-class User {
-    name: string;
-    age: number;
+enum PaymentStatus {
+    Holded,
+    Processed, 
+    Reversed
+}
 
-    constructor()
-    constructor(age: number)
-    constructor(name: string)
-    constructor(nameOrAge?:string | number) {
-        if (typeof nameOrAge === 'string') {
-            this.name = nameOrAge;
-        } else if (typeof nameOrAge === 'number') {
-            this.age = nameOrAge;
+class Payment {
+    id: number;
+    status: PaymentStatus;
+    createdAt: Date;
+    updatedAt: Date;
+
+    constructor(id: number) {
+        this.id = id;
+        this.createdAt = new Date();
+        this.status = PaymentStatus.Holded;
+    }
+    getPaymentLIfeTime(): number {
+        return new Date().getTime() - this.createdAt.getTime(); 
+    }
+
+    unholdPayment() {
+        if (this.status == PaymentStatus.Processed) {
+            throw new Error('платеж не может быть возвращен');
         }
+        this.status = PaymentStatus.Reversed;
+        this.updatedAt = new Date();
     }
 }
 
-const user = new User('Вася');
-const user1 = new User(22);
+const payment = new Payment(1);
+payment.unholdPayment();
+console.log(payment);
+const time = payment.getPaymentLIfeTime();
+console.log(time);
